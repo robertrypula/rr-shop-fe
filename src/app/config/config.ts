b0894d92d655aacc4713c91db2689301ccfa7dab
Robert Rypuła <robert.rypula@gmail.com>
2020-01-11 21:04:05 +0100

    Adds custom fonts and more layout functionality

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,5 @@
 export const CURRENCY = 'zł'; // TODO make it dynamic in future
-export const CLOUD_HIDE_DELAY = 10000;
+export const CLOUD_HIDE_DELAY = 2000;
 export const CLOUD_SHOW_DELAY = 250;
 export const BASKET_BAR_SUCCESS_MESSAGE_HIDE_DELAY = 5000;
 export const NO_PICTURE_FILENAME = '-no-picture-.jpg';

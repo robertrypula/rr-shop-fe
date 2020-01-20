a28a4573f2be55940cbc6b047cc61e3fda2e3c4a
Robert Rypuła <robert.rypula@gmail.com>
2020-01-20 23:52:12 +0100

    Adds Search page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,12 +11,14 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { NotFoundModule } from './pages/not-found/not-found.module';
 import { ProductComponent } from './pages/product/product.component';
 import { ProductModule } from './pages/product/product.module';
+import { SearchComponent } from './pages/search/search.component';
+import { SearchModule } from './pages/search/search.module';
 
 const routes: Routes = [
   { path: '', component: MainComponent, pathMatch: 'full' },
   { path: 'basket', component: BasketComponent },
   { path: 'product/:productIdWithSlug', component: ProductComponent },
-  { path: 'search/:keywords', component: CategoryComponent },
+  { path: 'search/:keywords', component: SearchComponent },
   { path: ':categoryIdWithSlug', component: CategoryComponent },
   // {
   //   path: 'heroes',
@@ -27,7 +29,15 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), ProductModule, CategoryModule, NotFoundModule, MainModule, BasketModule],
+  imports: [
+    RouterModule.forRoot(routes),
+    ProductModule,
+    CategoryModule,
+    NotFoundModule,
+    MainModule,
+    BasketModule,
+    SearchModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
---
+++ src/app/pages/search/search.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rr-shop-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+  public constructor() {}
+
+  public ngOnInit(): void {}
+}

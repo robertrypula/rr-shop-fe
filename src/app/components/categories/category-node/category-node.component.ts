2e024b0d2b72d1ba243ffb064dacaf4bc3c82fcb
Robert Rypuła <robert.rypula@gmail.com>
2020-02-16 18:59:46 +0100

    Shows numer of products in each category (left side menu)

--- src/app/components/categories/category-node/category-node.component.ts
+++ src/app/components/categories/category-node/category-node.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 
 import { Category } from '../../../models/category.model';
 import { CategoryService } from '../../../services/category.service';
+import { ProductService } from '../../../services/product.service';
 
 @Component({
   selector: 'rr-shop-category-node',
@@ -12,14 +13,19 @@ import { CategoryService } from '../../../services/category.service';
 })
 export class CategoryNodeComponent implements OnInit {
   @Input()
-  public parentId: number;
+  public category: Category;
 
-  public categories$: Observable<Category[]>;
+  @Input()
+  public isRoot = false;
 
-  public constructor(protected categoryService: CategoryService) {}
+  public childCategories$: Observable<Category[]>;
+  public something$: Observable<number>;
+
+  public constructor(protected categoryService: CategoryService, protected productService: ProductService) {}
 
   public ngOnInit(): void {
-    this.categories$ = this.categoryService.categoriesByParentId$(this.parentId);
+    this.childCategories$ = this.categoryService.categoriesByParentId$(this.category.id);
+    this.something$ = this.productService.productsCountFromCategoryAndItsChildrenByCategoryId$(this.category.id);
   }
 
   public trackBy(index: number, item: Category): string {

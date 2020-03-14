9a37c0edd1814d98d3ff670dc24fef51da4ec652
Robert Rypuła <robert.rypula@gmail.com>
2020-03-14 13:25:46 +0100

    Fixes the bug related to frozed loading screen

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -29,7 +29,7 @@ export const reducers: ActionReducerMap<State> = {
 
 export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
   return localStorageSync({
-    keys: ['order'],
+    keys: [{ order: ['entities', 'lastOrderItemId', 'lastOrderId'] }],
     rehydrate: true
   })(reducer);
 }

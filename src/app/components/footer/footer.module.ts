0cff31945a64601472a685ee2bb1741f0cfdbd82
Robert Rypuła <robert.rypula@gmail.com>
2020-04-05 18:15:18 +0200

    Text and header layout changes

--- src/app/components/footer/footer.module.ts
+++ src/app/components/footer/footer.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
 import { RouterModule } from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
 
 import { FooterGroupComponent } from './footer-group/footer-group.component';
 import { DirectivesModule } from '../../directives/directives.module';
@@ -10,6 +11,6 @@ import { PipesModule } from '../../pipes/pipes.module';
 @NgModule({
   declarations: [FooterComponent, FooterGroupComponent],
   exports: [FooterComponent],
-  imports: [CommonModule, RouterModule, DirectivesModule, PipesModule]
+  imports: [CommonModule, RouterModule, DirectivesModule, PipesModule, MarkdownModule]
 })
 export class FooterModule {}

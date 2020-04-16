bfe98f06702f8b7bacf8cdbdd020a612f485181d
Robert Rypuła <robert.rypula@gmail.com>
2020-04-16 21:37:13 +0200

    Renames one component directory

--- src/app/pages/potential-order/potential-order.module.ts
+++ src/app/pages/potential-order/potential-order.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PotentialOrderComponent } from './potential-order.component';
-import { OrderItemsOverviewModule } from '../../components/order-overview/order-items-overview.module';
+import { OrderItemsOverviewModule } from '../../components/order-items-overview/order-items-overview.module';
 import { DeliveryOverviewModule } from '../../components/delivery-overview/delivery-overview.module';
 import { PaymentOverviewModule } from '../../components/payment-overview/payment-overview.module';
 import { ClickableActionModule } from '../../components/clickable-action/clickable-action.module';

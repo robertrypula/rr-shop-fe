dd8a9a4ee0591b2cf43adb0b43b36fc70ed76073
Robert Rypuła <robert.rypula@gmail.com>
2020-04-21 19:44:33 +0200

    Formats and improves in admin tables

--- src/app/admin/pages/admin-order-list/admin-order-list.component.ts
+++ src/app/admin/pages/admin-order-list/admin-order-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AdminBaseComponent } from '../admin-base-component.class';
+import { Status } from '../../../models/order.model';
 
 /**
  * It's not following any of the best practices but I wrote this Admin in 1 hour :)
@@ -13,6 +14,7 @@ import { AdminBaseComponent } from '../admin-base-component.class';
   styleUrls: ['./admin-order-list.component.scss']
 })
 export class AdminOrderListComponent extends AdminBaseComponent implements OnInit {
+  public readonly Status = Status;
   public ngOnInit(): void {
     this.load('order');
   }

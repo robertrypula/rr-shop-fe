13397a3321d7f2d0ec9c94a06f49bdb632c54f4e
Robert Rypuła <robert.rypula@gmail.com>
2020-04-26 14:54:50 +0200

    Fixes in main motto

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { ClickableActionModule } from '../../components/clickable-action/clickable-action.module';
 import { ProductsListModule } from '../../components/products-list/products-list.module';
 
 import { MainComponent } from './main.component';
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [CommonModule, ProductsListModule, ClickableActionModule]
+  imports: [CommonModule, ProductsListModule]
 })
 export class MainModule {}

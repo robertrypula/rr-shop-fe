f8f26737180b6b8763edac35342be68a9447edb7
Robert Rypuła <robert.rypula@gmail.com>
2020-05-17 18:14:27 +0200

    Fixes linter issue

--- src/app/components/order-items-overview/order-items-overview.module.ts
+++ src/app/components/order-items-overview/order-items-overview.module.ts
@@ -1,19 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
 
 import { ClickableActionModule } from '../clickable-action/clickable-action.module';
 import { ImageModule } from '../image/image.module';
 import { NoContentModule } from '../no-content/no-content.module';
 import { PipesModule } from '../../pipes/pipes.module';
 
-import { MarkdownModule } from "ngx-markdown";
 import { OrderItemOverviewComponent } from './order-item-overview/order-item-overview.component';
 import { OrderItemsOverviewComponent } from './order-items-overview.component';
 
 @NgModule({
   declarations: [OrderItemsOverviewComponent, OrderItemOverviewComponent],
   exports: [OrderItemsOverviewComponent],
-  imports: [CommonModule, PipesModule, ClickableActionModule, RouterModule, ImageModule, NoContentModule, MarkdownModule]
+  imports: [
+    CommonModule,
+    PipesModule,
+    ClickableActionModule,
+    RouterModule,
+    ImageModule,
+    NoContentModule,
+    MarkdownModule
+  ]
 })
 export class OrderItemsOverviewModule {}

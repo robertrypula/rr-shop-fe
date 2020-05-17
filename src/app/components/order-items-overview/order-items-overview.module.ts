71fe5bf930782c18bedd7921d271cc5fb927a4a8
Robert Rypuła <robert.rypula@gmail.com>
2020-05-17 18:12:19 +0200

    Adds validation of deliveryType blocking (FE part)

--- src/app/components/order-items-overview/order-items-overview.module.ts
+++ src/app/components/order-items-overview/order-items-overview.module.ts
@@ -7,12 +7,13 @@ import { ImageModule } from '../image/image.module';
 import { NoContentModule } from '../no-content/no-content.module';
 import { PipesModule } from '../../pipes/pipes.module';
 
+import { MarkdownModule } from "ngx-markdown";
 import { OrderItemOverviewComponent } from './order-item-overview/order-item-overview.component';
 import { OrderItemsOverviewComponent } from './order-items-overview.component';
 
 @NgModule({
   declarations: [OrderItemsOverviewComponent, OrderItemOverviewComponent],
   exports: [OrderItemsOverviewComponent],
-  imports: [CommonModule, PipesModule, ClickableActionModule, RouterModule, ImageModule, NoContentModule]
+  imports: [CommonModule, PipesModule, ClickableActionModule, RouterModule, ImageModule, NoContentModule, MarkdownModule]
 })
 export class OrderItemsOverviewModule {}
